1ef892e8b3f28b39f27d56a72c966dcb4065edaf
yene2024 <yeneishla1524@gmail.com>
2023-11-24 13:49:59 -0400

    Adding header file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+
+
+int _printf(const char*, ...);
+/**int _putchar(char);
+int print_char(char);
+int print_str(char*);
+int print_d(int);
+int print_rot13(int);
+int print_rev(int);**/
+
+#endif
