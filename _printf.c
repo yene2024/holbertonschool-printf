db4932fa39414d8e9f5e1c34ff4d689479a0bc1d
yene2024 <yeneishla1524@gmail.com>
2023-11-24 20:44:39 -0400

    Addressed warnings and improved the code documentation.

--- _printf.c
+++ _printf.c
@@ -4,57 +4,119 @@
 #include "main.h"
 
 /**
- * _printf - Custom printf function
+ * _printf - Custom printf function, calls other functions.
  * @format: Format string containing conversion specifiers
- *
+ * @args: Variable arguments list
+ * @count: Number of characters printed
  * Return: Number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-    int chara_print = 0;
-    int count = 0;
-    va_list list_of_args;
+	int chara_print = 0;
+	int count = 0;
+	va_list args;
 
-    if (format == NULL)
-    {
-        return (-1);
-    }
+	if (!format)
+	{
+		return (-1);
+	}
 
-    va_start(list_of_args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format != '%' || (*(format + 1) == '%' && format++))
-        {
-            write(1, format, 1);
-            chara_print++;
-            count++;
-        }
-        else if (*(format + 1) == 'c')
-        {
-            char c = va_arg(list_of_args, int);
-            write(1, &c, 1);
-            chara_print++;
-            count++;
-            format++;
-        }
-        else if (*(format + 1) == 's')
-        {
-            char *str = va_arg(list_of_args, char*);
-            int str_len = 0;
+	while (*format)
+	{
+		if (*format == '%' && (++format))
+		{
+			int (*handlers[256])(va_list, int *) = { NULL };
+			handlers[(unsigned char)'s'] = print_string;
+			handlers[(unsigned char)'c'] = print_char;
+			handlers[(unsigned char)'d'] = handlers[(unsigned char)'i'] = print_int;
 
-            while (str[str_len] != '\0')
-            {
-                write(1, &str[str_len], 1);
-                chara_print++;
-                count++;
-                str_len++;
-            }
-            format++;
-        }
-        format++;
-    }
+			if (handlers[(unsigned char)(*format)])
+			{
+				handlers[(unsigned char)(*format)](args, &count);
+			}
+			else
+			{
+				write(1, format - 1, 2);
+				chara_print += 2;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			chara_print++;
+			count++;
+			format++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
 
-    va_end(list_of_args);
-    return count;
+/**
+ * print_string - function to print a string
+ * @str: String to be printed
+ *
+ * Return: count
+ */
+
+int print_string(va_list args, int *count)
+{
+	char *str = va_arg(args, char *);
+
+	while (*str)
+	{
+		write(1, str++, 1);
+		(*count)++;
+	}
+	return (*count);
+}
+
+/**
+ * print_char - function to print a character
+ * @ch: Character to be printed
+ *
+ * Return: count
+ */
+
+int print_char(va_list args, int *count)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	(*count)++;
+	return (*count);
+}
+
+/**
+ * print_int - function to print an integer
+ * @num: Integer to be printed
+ *
+ * Return: count
+ */
+
+int print_int(va_list args, int *count)
+{
+	int num = va_arg(args, int);
+	char buffer[20];
+	int len = 0;
+
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		(*count)++;
+		num = -num;
+	}
+
+	do {
+		buffer[len++] = num % 10 + '0';
+	} while (num /= 10);
+
+	while (len)
+	{
+		write(1, &buffer[--len], 1);
+		(*count)++;
+	}
+	return (*count);
 }
