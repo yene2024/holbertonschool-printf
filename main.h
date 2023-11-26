8f778bff7a20eb3c23214df3f3b0e87d70b21f7a
yene2024 <yeneishla1524@gmail.com>
2023-11-26 14:42:14 -0400

    Adding print_rev function to header file

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 int _printf(const char *format, ...);
 char _putchar(char c);
 int print_char(va_list args, int *count);
 int print_string(va_list args, int *count);
 int print_int(va_list args, int *count);
+int print_rev(va_list args, int *count);
 #endif
